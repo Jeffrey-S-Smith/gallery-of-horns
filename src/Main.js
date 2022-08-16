ed5f49840fbbeb7b0a45746a228ab5318c446919
Jeffrey Smith <jeffrey_s_smith@live.com>
2022-08-16 15:25:10 -0700

    first feature

--- src/Main.js
+++ src/Main.js
@@ -7,11 +7,12 @@ import data from './data.json';
 class Main extends React.Component {
   render() {
     let beastArr = [];
-    data.forEach((beast) => {
+    data.forEach((beast,indx) => {
     beastArr.push(<HornedBeast
     title = {beast.title}
     image_url = {beast.image_url}
     description = {beast.description}
+    key={indx}
     />);
     });
       return (
